5e071a6ccf506ed9dfcaf479a5540b92292106c4
ole4ka140592 <ole4ka140592@gmail.com>
2022-03-28 01:05:03 +0300

    changes in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.BASE_URL}>
+    <HashRouter basename={process.env.BASE_URL}>
         <App/>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
